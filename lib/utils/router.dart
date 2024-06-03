95a00e6798ef621608cbf2a8037c741ad58c3a50
Miller Adulu <milleradulu@gmail.com>
2024-06-03 14:27:56 +0300

    Fix formatting

--- lib/utils/router.dart
+++ lib/utils/router.dart
@@ -1,9 +1,5 @@
-import 'package:app/decision.dart';
-import 'package:app/features/auth/auth.dart';
-import 'package:app/features/home/landing.dart';
 import 'package:app/utils/router.gr.dart';
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter/material.dart';
 
 @AutoRouterConfig()
 class PRFSuperAppRouter extends $PRFSuperAppRouter {
