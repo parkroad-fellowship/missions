655383d60fa42ff4679afacd9c3a01b65c216926
Miller Adulu <milleradulu@gmail.com>
2024-06-07 22:48:29 +0300

    Open Google map in the app

--- lib/features/home/missions/mission_details/widgets/mission_details/_handset.dart
+++ lib/features/home/missions/mission_details/widgets/mission_details/_handset.dart
@@ -1,6 +1,8 @@
 import 'package:app/l10n/l10n.dart';
 import 'package:app/models/prf_mission.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MissionDetailsViewHandset extends StatefulWidget {
   const MissionDetailsViewHandset({
@@ -43,7 +45,35 @@ class _MissionDetailsViewHandsetState extends State<MissionDetailsViewHandset> {
           const Divider(),
           ListTile(
             contentPadding: EdgeInsets.zero,
-            title: Text(l10n.address),
+            title: Row(
+              children: [
+                Text(l10n.address),
+                const Spacer(),
+                IconButton(
+                  onPressed: () async {
+                    final school = mission.school!;
+                    // final uri = Uri.parse(
+                    //   'geo:'
+                    //   'q=${school.latitude},${school.longitude}&mode=d',
+                    // );
+                    final uri = Uri(
+                      scheme: 'geo',
+                      host: '0,0',
+                      queryParameters: {
+                        'q': '${school.latitude},${school.longitude}',
+                        'mode': 'd',
+                      },
+                    );
+                    try {
+                      await launchUrl(uri);
+                    } catch (e) {
+                      Logger().e(e);
+                    }
+                  },
+                  icon: const Icon(Icons.map_rounded),
+                ),
+              ],
+            ),
             subtitle: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
