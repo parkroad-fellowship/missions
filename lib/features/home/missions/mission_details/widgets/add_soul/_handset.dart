cc731da90d1334fd95bfec96b1bef81072659df2
Miller Adulu <milleradulu@gmail.com>
2024-06-11 17:41:06 +0300

    Bump app version fix formatting

--- lib/features/home/missions/mission_details/widgets/add_soul/_handset.dart
+++ lib/features/home/missions/mission_details/widgets/add_soul/_handset.dart
@@ -1,6 +1,5 @@
 import 'package:app/features/home/missions/cubit/add_soul_cubit.dart';
 import 'package:app/features/home/missions/cubit/get_class_groups_cubit.dart';
-import 'package:app/features/home/missions/cubit/get_souls_cubit.dart';
 import 'package:app/l10n/l10n.dart';
 import 'package:app/models/prf_class_group.dart';
 import 'package:app/utils/_index.dart';
