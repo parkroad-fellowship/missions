e85490285adf3ddc8126f55c5e14762e565c2497
Miller Adulu <milleradulu@gmail.com>
2025-02-05 16:08:18 +0300

    Return all the suggested schools for non committee members

--- lib/features/home/mission_ground_suggestions/cubit/get_mission_ground_suggestions_cubit.dart
+++ lib/features/home/mission_ground_suggestions/cubit/get_mission_ground_suggestions_cubit.dart
@@ -32,10 +32,12 @@ class GetMissionGroundSuggestionsCubit
       final missionGroundSuggestions =
           await _missionGroundsService.getMissionGroundSuggestions(
         suggestorUlid: viewAnyMissionGrounds ? null : member.ulid,
-        statuses: [
-          PRFMissionGroundSuggestionStatus.pending,
-          PRFMissionGroundSuggestionStatus.initiatedContact,
-        ],
+        statuses: viewAnyMissionGrounds
+            ? [
+                PRFMissionGroundSuggestionStatus.pending,
+                PRFMissionGroundSuggestionStatus.initiatedContact,
+              ]
+            : [],
       );
 
       emit(
