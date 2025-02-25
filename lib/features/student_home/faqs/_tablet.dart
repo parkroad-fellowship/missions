4997272d27b75f6e08a9cdd4f8ee4855df14b458
Miller Adulu <milleradulu@gmail.com>
2025-02-26 01:00:41 +0300

    Add a tablet-friendly student FAQ page

---
+++ lib/features/student_home/faqs/_tablet.dart
@@ -0,0 +1,304 @@
+import 'package:app/features/student_home/faqs/cubit/get_faqs_cubit.dart';
+import 'package:app/l10n/l10n.dart';
+import 'package:app/models/local/prf_faq.dart';
+import 'package:app/models/local/prf_faq_category.dart';
+import 'package:app/utils/_index.dart';
+import 'package:app/widgets/_index.dart';
+import 'package:app/widgets/linear_progress_indicator.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:logger/logger.dart';
+
+class FAQPageTablet extends StatefulWidget {
+  const FAQPageTablet({super.key});
+
+  @override
+  State<FAQPageTablet> createState() => _FAQPageTabletState();
+}
+
+class _FAQPageTabletState extends State<FAQPageTablet> {
+  PRFLocalFaqCategory? _selectedCategory;
+  String? _searchQuery;
+
+  final _searchDeboucer = Debouncer(milliseconds: 1 * 1000);
+
+  @override
+  void initState() {
+    context.read<GetFaqsCubit>().getFaqs();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    Misc.initDimensions(context);
+
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.w),
+          child: CustomScrollView(
+            slivers: [
+              // Start Navigation Bar
+              SliverAppBar(
+                automaticallyImplyLeading: false,
+                backgroundColor: Colors.white,
+                surfaceTintColor: Colors.white,
+                pinned: true,
+                flexibleSpace: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.w),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            color: PRFApp.theme().kPrimaryColorV2,
+                            width: 1.w,
+                          ),
+                        ),
+                        child: IconButton(
+                          icon: const Icon(Icons.arrow_back_ios),
+                          padding: const EdgeInsets.only(left: 8),
+                          onPressed:
+                              () => context.router.popUntilRouteWithPath(
+                                PRFSuperAppRouter.studentLandingRoute,
+                              ),
+                        ),
+                      ),
+                      const Spacer(),
+                      Text(
+                        l10n.questions,
+                        style: PRFText.theme().displayLarge?.copyWith(
+                          fontSize: 56.sp,
+                        ),
+                      ),
+                      const Spacer(),
+                      Padding(
+                        padding: EdgeInsets.only(right: 16.w),
+                        child: const Visibility(
+                          child: Icon(Icons.abc, color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              // End Navigation Bar
+              SliverToBoxAdapter(child: SizedBox(height: 48.h)),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.w),
+                  child: TextField(
+                    onChanged: (value) {
+                      setState(() {
+                        _searchQuery = value;
+                      });
+                      Logger().i('Search Query: $_searchQuery');
+
+                      _searchDeboucer.run(() {
+                        context.read<GetFaqsCubit>().getFaqs(
+                          categoryUlid:
+                              _selectedCategory?.ulid ??
+                              _selectedCategory?.ulid,
+                          query:
+                              _searchQuery != null ||
+                                      (_searchQuery?.isNotEmpty ?? false)
+                                  ? _searchQuery
+                                  : null,
+                        );
+                      });
+                    },
+                    decoration: InputDecoration(
+                      hintText: l10n.whatWouldYouLikeToKnow,
+                      suffixIconColor: PRFApp.theme().kPrimaryColorV2,
+                      suffixIcon: Container(
+                        margin: const EdgeInsets.only(right: 8),
+                        child: const Icon(Icons.search),
+                      ),
+                      hintStyle: PRFText.theme().bodyMedium?.copyWith(
+                        fontWeight: FontWeight.bold,
+                        color: PRFApp.theme().kPrimaryColorV2.withAlpha(200),
+                        fontSize: 12,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: PRFApp.theme().kAccent2BackgroundColor,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: PRFApp.theme().kAccent2BackgroundColor,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                    ),
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(child: SizedBox(height: 16.h)),
+              SliverToBoxAdapter(
+                child: BlocBuilder<GetFaqsCubit, GetFaqsState>(
+                  builder:
+                      (context, state) => state.maybeWhen(
+                        loading:
+                            () =>
+                                const Center(child: LinearProgressIndicator()),
+                        orElse: () => const SizedBox.shrink(),
+                      ),
+                ),
+              ),
+              SliverToBoxAdapter(child: SizedBox(height: 16.h)),
+              SliverToBoxAdapter(
+                child: FaqCategoriesPreview(
+                  onCategorySelected: (newValue) {
+                    setState(() {
+                      _selectedCategory = newValue;
+                    });
+                    Logger().i('Selected Category: $_selectedCategory');
+                    context.read<GetFaqsCubit>().getFaqs(
+                      categoryUlid: _selectedCategory?.ulid,
+                      query:
+                          _searchQuery != null ||
+                                  (_searchQuery?.isNotEmpty ?? false)
+                              ? _searchQuery
+                              : null,
+                    );
+                  },
+                ),
+              ),
+              SliverToBoxAdapter(child: SizedBox(height: 16.h)),
+              BlocBuilder<GetFaqsCubit, GetFaqsState>(
+                builder: (context, state) {
+                  return state.maybeWhen(
+                    orElse: SizedBox.shrink,
+                    loading:
+                        () => const SliverToBoxAdapter(
+                          child: PRFLinearProgressIndicator(),
+                        ),
+                    error:
+                        (message) => SliverFillRemaining(
+                          child: Center(child: Text(message)),
+                        ),
+                    loaded: (faqs) {
+                      if (faqs.isEmpty) {
+                        return SliverFillRemaining(
+                          child: RefreshIndicator(
+                            onRefresh:
+                                () => context.read<GetFaqsCubit>().getFaqs(),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Spacer(),
+                                const Icon(Icons.directions_walk),
+                                Center(
+                                  child: Text(
+                                    l10n.noFaqs,
+                                    style: PRFText.theme().headlineMedium!
+                                        .copyWith(
+                                          color: PRFApp.theme().kDullGreyColor,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                  ),
+                                ),
+                                const SizedBox(height: 10),
+                                SizedBox(
+                                  height:
+                                      MediaQuery.sizeOf(context).height * 0.05,
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      Text(
+                                        l10n.pleaseWait,
+                                        style: PRFText.theme().displayLarge!
+                                            .copyWith(
+                                              color:
+                                                  PRFApp.theme()
+                                                      .kPrimaryColorV2,
+                                              fontSize: 14,
+                                            ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                const Spacer(),
+                              ],
+                            ),
+                          ),
+                        );
+                      }
+                      return SliverList.separated(
+                        itemCount: faqs.length,
+                        separatorBuilder:
+                            (context, index) => SizedBox(height: 8.h),
+                        itemBuilder:
+                            (context, index) => FaqCard(faq: faqs[index]),
+                      );
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FaqCard extends StatelessWidget {
+  const FaqCard({required this.faq, super.key});
+
+  final PRFLocalFaq faq;
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.sizeOf(context).width;
+    return Animate(
+      effects: const [SaturateEffect()],
+      child: Stack(
+        children: [
+          Container(
+            width: width,
+            padding: EdgeInsets.symmetric(horizontal: 50.w, vertical: 60.h),
+            margin: EdgeInsets.symmetric(horizontal: 16.w),
+            decoration: BoxDecoration(
+              color: PRFApp.theme().kSecondaryColorV2.withValues(alpha: .3),
+              borderRadius: BorderRadius.circular(48.r),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  faq.question,
+                  style: PRFText.theme().titleLarge!.copyWith(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(height: 8.h),
+                Text(
+                  faq.answer,
+                  style: PRFText.theme().bodySmall!.copyWith(
+                    color: PRFApp.theme().kBlackColor,
+                    fontSize: 14,
+                  ),
+                ),
+                SizedBox(height: 8.h),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/student_home/faqs/faqs.dart
+++ lib/features/student_home/faqs/faqs.dart
@@ -1,4 +1,5 @@
 import 'package:app/features/student_home/faqs/_handset.dart';
+import 'package:app/features/student_home/faqs/_tablet.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_adaptive_ui/flutter_adaptive_ui.dart';
@@ -10,7 +11,7 @@ class FAQPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AdaptiveBuilder(
-      defaultBuilder: (_, __) => const FAQPageHandset(),
+      defaultBuilder: (_, __) => const FAQPageTablet(),
       layoutDelegate: AdaptiveLayoutDelegateWithMinimallScreenType(
         handset: (_, __) => const FAQPageHandset(),
       ),
