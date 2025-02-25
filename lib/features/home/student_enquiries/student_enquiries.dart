4b1a5c8c248638106b043a15e9574a19d096d207
Miller Adulu <milleradulu@gmail.com>
2025-02-26 00:04:50 +0300

    Add tablet friendly student enquiry page

--- lib/features/home/student_enquiries/student_enquiries.dart
+++ lib/features/home/student_enquiries/student_enquiries.dart
@@ -1,4 +1,5 @@
 import 'package:app/features/home/student_enquiries/_handset.dart';
+import 'package:app/features/home/student_enquiries/_tablet.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_adaptive_ui/flutter_adaptive_ui.dart';
@@ -10,7 +11,7 @@ class StudentEnquiriesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AdaptiveBuilder(
-      defaultBuilder: (_, __) => const StudentEnquiriesPageHandset(),
+      defaultBuilder: (_, __) => const StudentEnquiriesPageTablet(),
       layoutDelegate: AdaptiveLayoutDelegateWithMinimallScreenType(
         handset: (_, __) => const StudentEnquiriesPageHandset(),
       ),
