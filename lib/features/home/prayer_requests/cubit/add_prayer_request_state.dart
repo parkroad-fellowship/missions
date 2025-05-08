0e24f72106c372b3693ff8dd9beef727f9cc9915
tonnycoder001 <tonnykahihia@gmail.com>
2025-05-08 11:32:48 +0300

    Rename state `success` to `loaded`

--- lib/features/home/prayer_requests/cubit/add_prayer_request_state.dart
+++ lib/features/home/prayer_requests/cubit/add_prayer_request_state.dart
@@ -4,7 +4,7 @@ part of 'add_prayer_request_cubit.dart';
 class AddPrayerRequestState with _$AddPrayerRequestState {
   const factory AddPrayerRequestState.initial() = _Initial;
   const factory AddPrayerRequestState.loading() = _Loading;
-  const factory AddPrayerRequestState.success(PRFPrayerRequest prayerRequest) =
-      _Success;
+  const factory AddPrayerRequestState.loaded(PRFPrayerRequest prayerRequest) =
+      _Loaded;
   const factory AddPrayerRequestState.error(String message) = _Error;
 }
