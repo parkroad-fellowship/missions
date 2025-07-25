655f8a3eb4458bf617122954f78047167cedd644
Miller Adulu <milleradulu@gmail.com>
2025-07-25 21:40:02 +0300

    Revert to production values

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -8,13 +8,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // TODO(MillerAdulu): Revert to production values
   PRFSuperAppConfig(
     values: PRFSuperAppValues(
-      hiveBox: 'prf-super-app-stg',
-      baseDomain: 'dev.api.parkroadfellowship.org',
+      hiveBox: 'prf-missions--${Misc.getSluggedAppVersion()}',
+      baseDomain: 'api.parkroadfellowship.org',
       urlScheme: 'https',
-      socketDomain: 'dev.ws.parkroadfellowship.org',
+      socketDomain: 'ws.parkroadfellowship.org',
       socketKey: 'yvnlkaqadqiadutrs9sa',
       socketScheme: 'wss',
       socketPort: 443,
@@ -22,20 +21,6 @@ Future<void> main() async {
           'DefaultEndpointsProtocol=https;AccountName=prfcorestorage;AccountKey=oizfzMYG6gsjQWTfix8V/50Jh40qCg93DzNiFok/DxJjDOhffzM0TA4TNOV4TYqU1QONfaQOrrs7+ASteXMXPA==;EndpointSuffix=core.windows.net',
     ),
   );
-
-  // PRFSuperAppConfig(
-  //   values: PRFSuperAppValues(
-  //     hiveBox: 'prf-missions--${Misc.getSluggedAppVersion()}',
-  //     baseDomain: 'api.parkroadfellowship.org',
-  //     urlScheme: 'https',
-  //     socketDomain: 'ws.parkroadfellowship.org',
-  //     socketKey: 'yvnlkaqadqiadutrs9sa',
-  //     socketScheme: 'wss',
-  //     socketPort: 443,
-  //     azureConnString:
-  //         'DefaultEndpointsProtocol=https;AccountName=prfcorestorage;AccountKey=oizfzMYG6gsjQWTfix8V/50Jh40qCg93DzNiFok/DxJjDOhffzM0TA4TNOV4TYqU1QONfaQOrrs7+ASteXMXPA==;EndpointSuffix=core.windows.net',
-  //   ),
-  // );
 
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
