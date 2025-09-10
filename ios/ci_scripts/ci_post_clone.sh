ef38928193773cb2aadeeb2c90dbba1be0dd272d
Miller Adulu <milleradulu@gmail.com>
2025-09-10 08:29:11 +0300

    Update post clone iOS script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -39,7 +39,16 @@ flutter precache --ios
 flutter pub get
 
 # Configure Flutter project for release mode
-flutter build ios --config-only --release
+if [ "$app_env" = "production" ]; then
+  flutter build ios --config-only --release --flavor production -t lib/main_production.dart
+elif [ "$app_env" = "staging" ]; then
+  flutter build ios --config-only --release --flavor staging -t lib/main_staging.dart
+elif [ "$app_env" = "development" ]; then
+  flutter build ios --config-only --release --flavor development -t lib/main_development.dart
+else
+  echo "Unknown APP_ENV: $app_env"
+  exit 1
+fi
 
 # # Check for any formatting issues
 # dart format --set-exit-if-changed lib test
