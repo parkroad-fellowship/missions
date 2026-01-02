60b92b6a3bda45ed85826b648c5a7acbd604737e
Miller Adulu <milleradulu@gmail.com>
2026-01-02 18:05:05 +0300

    Fix the refund model relationship

--- lib/models/remote/prf_refund.dart
+++ lib/models/remote/prf_refund.dart
@@ -1,3 +1,4 @@
+import 'package:app/models/remote/prf_accounting_event.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'prf_refund.freezed.dart';
@@ -12,7 +13,7 @@ abstract class PRFRefund with _$PRFRefund {
     @JsonKey(name: 'confirmation_message') String confirmationMessage,
     @JsonKey(name: 'created_at') DateTime createdAt,
     @JsonKey(name: 'updated_at') DateTime updatedAt, {
-    @JsonKey(name: 'accounting_event') String? accountingEvent,
+    @JsonKey(name: 'accounting_event') PRFAccountingEvent? accountingEvent,
   }) = _PRFRefund;
 
   factory PRFRefund.fromJson(Map<String, dynamic> json) =>
