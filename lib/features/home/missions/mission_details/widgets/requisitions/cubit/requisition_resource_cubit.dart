38473062ee9a1fb492fdb9e02ccc7f1bad72e323
Miller Adulu <milleradulu@gmail.com>
2026-03-30 13:03:40 +0300

    Only show approved requisitions on the member view

--- lib/features/home/missions/mission_details/widgets/requisitions/cubit/requisition_resource_cubit.dart
+++ lib/features/home/missions/mission_details/widgets/requisitions/cubit/requisition_resource_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:app/enums/expense/prf_approval_status.dart';
 import 'package:app/models/remote/expense/prf_requisition.dart';
 import 'package:app/services/api/requisition_service.dart';
 import 'package:app/utils/crud/resource_cubit.dart';
@@ -13,6 +14,11 @@ class RequisitionResourceCubit extends ResourceCubit<PRFRequisition> {
   Future<void> loadForAccountingEvent({
     required String accountingEventUlid,
   }) async {
-    await loadAll(filters: {'accounting_event_ulid': accountingEventUlid});
+    await loadAll(
+      filters: {
+        'accounting_event_ulid': accountingEventUlid,
+        'approval_status': PRFApprovalStatus.approved.apiKey,
+      },
+    );
   }
 }
