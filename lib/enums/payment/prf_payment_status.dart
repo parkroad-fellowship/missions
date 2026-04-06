0f79314abd6185b1688227a45361ecc8c9cbccb8
Miller Adulu <milleradulu@gmail.com>
2026-04-06 17:19:09 +0300

    Fix typo

--- lib/enums/payment/prf_payment_status.dart
+++ lib/enums/payment/prf_payment_status.dart
@@ -24,7 +24,7 @@ enum PRFPaymentStatus {
       case PRFPaymentStatus.cancelled:
         return 'Cancelled';
       case PRFPaymentStatus.failed:
-        return 'Failied';
+        return 'Failed';
     }
   }
 }
